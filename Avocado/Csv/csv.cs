43080d1e18b4b1fdd889b4ff618856d5b2aeddc3
Esteban Ramirez <netssrmrz@yahoo.com.au>
2019-02-21 23:18:52 +1100

    create basic csv functionality

--- Avocado/Csv/csv.cs
+++ Avocado/Csv/csv.cs
@@ -1,47 +1,175 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
 namespace Avocado
 {
   public class CsvReader
   {
     System.IO.TextReader dataSource;
+    public bool hasTitleRow;
 
     public CsvReader(string rawData)
     {
       this.dataSource = new System.IO.StringReader(rawData);
     }
 
-    /*public T ReadLine<T>() where T: new()
+    public CsvReader(System.IO.StreamReader file)
+    {
+      this.dataSource = file;
+    }
+
+    public T ReadLine<T>() where T: new()
     {
       T obj = default(T);
       string dataLine;
       string[] dataTokens;
 
       dataLine = this.dataSource.ReadLine();
-      dataTokens = StringToTokens(dataLine);
+      if (dataLine != null)
+      {
+        dataTokens = StringToTokens(dataLine);
 
-      obj = new T();
-      TokensToObj(dataTokens, obj);
-      
+        obj = new T();
+        TokensToObj(dataTokens, obj);
+      }
       return obj;
-    }*/
+    }
 
-    public static string[] StringToTokens(string dataLine)
+    public T[] ReadLines<T>() where T : new()
     {
-      string[] res = null;
+      System.Collections.Generic.List<T> objs;
+      T obj;
+      T[] res = null;
 
-      res = dataLine.Split(',');
+      objs = new System.Collections.Generic.List<T>();
+      if (this.hasTitleRow)
+        this.dataSource.ReadLine();
+      do
+      {
+        obj = this.ReadLine<T>();
+        if (obj != null)
+          objs.Add(obj);
+      }
+      while (this.dataSource.Peek()!=-1);
+
+      if (objs.Count > 0)
+        res = objs.ToArray();
 
       return res;
     }
 
-    /*public static void TokensToObj(string[] dataTokens, Object obj)
+    public static string[] StringToTokens(string dataLine)
     {
+      string[] tokens = null;
+      int c;
 
-    }*/
+      if (!string.IsNullOrEmpty(dataLine))
+      {
+        tokens = dataLine.Split(',');
+        for (c=0; c<tokens.Length; c++)
+          tokens[c] = tokens[c].Trim();
+      }
+
+      return tokens;
+    }
+
+    public static void TokensToObj(string[] tokens, Object obj)
+    {
+      int c;
+      string token;
+      System.Reflection.FieldInfo field;
+
+      for (c=0; c< tokens.Length; c++)
+      {
+        token = tokens[c];
+        field = GetFieldWithColumnIdx(c, obj);
+        SetFieldValue(obj, field, token);
+      }
+    }
+
+    public static System.Reflection.FieldInfo GetFieldWithColumnIdx(int colIdx, Object obj)
+    {
+      System.Reflection.FieldInfo res = null;
+      System.Reflection.FieldInfo[] fields;
+      int c;
+      int? fieldColIdx;
+
+      fields = obj.GetType().GetFields();
+      for (c = 0; c < fields.Length; c++)
+      {
+        fieldColIdx = GetFieldColumnIdx(fields[c]);
+        if (fieldColIdx != null && fieldColIdx == colIdx)
+          res = fields[c];
+      }
+
+      return res;
+    }
+
+    public static int? GetFieldColumnIdx(System.Reflection.FieldInfo field)
+    {
+      int? res = null;
+      object[] attrs;
+
+      attrs = field.GetCustomAttributes(typeof(Avocado.Csv.Column), false);
+      if (attrs!=null && attrs.Length>0)
+        res = ((Avocado.Csv.Column)attrs[0]).colIdx;
+
+      return res;
+    }
+
+    public static void SetFieldValue(object obj, System.Reflection.FieldInfo field, string value)
+    {
+      object fieldValue=null;
+
+      if (field != null)
+      {
+        if (field.FieldType == typeof(int))
+          fieldValue = ToInt(value);
+        else if (field.FieldType == typeof(float))
+          fieldValue = ToFloat(value);
+        else if (field.FieldType == typeof(string))
+          fieldValue = value;
+        else if (field.FieldType == typeof(System.DateTime))
+          fieldValue = ToDateTime(value);
+
+        field.SetValue(obj, fieldValue);
+      }
+    }
+
+    public static System.DateTime? ToDateTime(string strValue)
+    {
+      System.DateTime parseValue;
+      System.DateTime? res = null;
+
+      if (System.DateTime.TryParse(strValue, out parseValue))
+        res = parseValue;
+
+      return res;
+    }
+
+    public static float? ToFloat(string strValue)
+    {
+      float parseValue;
+      float? res = null;
+
+      if (System.Single.TryParse(strValue, out parseValue))
+        res = parseValue;
+
+      return res;
+    }
+
+    public static int? ToInt(string strValue)
+    {
+      int parseValue;
+      int? res = null;
+
+      if (System.Int32.TryParse(strValue, out parseValue))
+        res = parseValue;
+
+      return res;
+    }
   }
 }
--- UnitTests/CSVTests.cs
+++ UnitTests/CSVTests.cs
@@ -8,21 +8,175 @@ namespace UnitTests
   {
     public class PlainObj1
     {
-      [Avocado.Csv.Column(1)]
+      [Avocado.Csv.Column(0)]
       public string name;
 
-      [Avocado.Csv.Column(2)]
+      [Avocado.Csv.Column(1)]
       public int age;
 
-      [Avocado.Csv.Column(3)]
+      [Avocado.Csv.Column(2)]
       public float height;
 
-      [Avocado.Csv.Column(4)]
+      [Avocado.Csv.Column(3)]
       public System.DateTime dob;
     }
 
-    /*[TestMethod]
-    public void Read_Single_Plain_Obj()
+    public class PlainObj2
+    {
+      [Avocado.Csv.Column(0)]
+      public string policyID;
+
+      [Avocado.Csv.Column(3)]
+      public float eq_site_limit;    }
+
+    [TestMethod]
+    public void StringToTokens_Basic()
+    {
+      string[] tokens;
+
+      tokens = Avocado.CsvReader.StringToTokens("Roger Ramjet, 47, 170.12, 13/11/1971");
+      Assert.AreEqual("Roger Ramjet", tokens[0]);
+      Assert.AreEqual("47", tokens[1]);
+      Assert.AreEqual("170.12", tokens[2]);
+      Assert.AreEqual("13/11/1971", tokens[3]);
+    }
+
+    [TestMethod]
+    public void GetFieldColumnIdx_Basic()
+    {
+      PlainObj1 obj;
+      System.Reflection.FieldInfo field;
+      int? colIdx;
+
+      obj = new PlainObj1();
+      obj.name = "Roger Ramjet";
+      obj.age = 47;
+      obj.height = 170.12f;
+      obj.dob = new System.DateTime(1971, 11, 13);
+
+      field = obj.GetType().GetField("age");
+      colIdx = Avocado.CsvReader.GetFieldColumnIdx(field);
+
+      Assert.AreEqual(1, colIdx);
+    }
+
+    [TestMethod]
+    public void GetFieldWithColumnIdx_Basic()
+    {
+      PlainObj1 obj;
+      System.Reflection.FieldInfo field;
+
+      obj = new PlainObj1();
+      field = Avocado.CsvReader.GetFieldWithColumnIdx(1, obj);
+
+      Assert.AreEqual("age", field.Name);
+    }
+
+    [TestMethod]
+    public void ToInt_Basic()
+    {
+      int? res;
+
+      res = Avocado.CsvReader.ToInt(null);
+      Assert.IsNull(res);
+
+      res = Avocado.CsvReader.ToInt("");
+      Assert.IsNull(res);
+
+      res = Avocado.CsvReader.ToInt("fred");
+      Assert.IsNull(res);
+
+      res = Avocado.CsvReader.ToInt("1.11");
+      Assert.IsNull(res);
+
+      res = Avocado.CsvReader.ToInt("1");
+      Assert.AreEqual(1, res);
+    }
+
+    [TestMethod]
+    public void ToFloat_Basic()
+    {
+      float? res;
+
+      res = Avocado.CsvReader.ToFloat(null);
+      Assert.IsNull(res);
+
+      res = Avocado.CsvReader.ToFloat("");
+      Assert.IsNull(res);
+
+      res = Avocado.CsvReader.ToFloat("fred");
+      Assert.IsNull(res);
+
+      res = Avocado.CsvReader.ToFloat("1");
+      Assert.AreEqual(1f, res);
+
+      res = Avocado.CsvReader.ToFloat("1.11");
+      Assert.AreEqual(1.11f, res);
+    }
+
+    [TestMethod]
+    public void ToDateTime_Basic()
+    {
+      System.DateTime? res;
+
+      res = Avocado.CsvReader.ToDateTime(null);
+      Assert.IsNull(res);
+
+      res = Avocado.CsvReader.ToDateTime("");
+      Assert.IsNull(res);
+
+      res = Avocado.CsvReader.ToDateTime("fred");
+      Assert.IsNull(res);
+
+      res = Avocado.CsvReader.ToDateTime("1");
+      Assert.IsNull(res);
+
+      res = Avocado.CsvReader.ToDateTime("13/11/1971");
+      Assert.AreEqual(new System.DateTime(1971, 11, 13), res);
+    }
+
+    [TestMethod]
+    public void SetFieldValue_Basic()
+    {
+      PlainObj1 obj;
+      System.Reflection.FieldInfo field;
+
+      obj = new PlainObj1();
+
+      field = obj.GetType().GetField("name");
+      Avocado.CsvReader.SetFieldValue(obj, field, "Roger Ramjet");
+      Assert.AreEqual("Roger Ramjet", obj.name);
+
+      field = obj.GetType().GetField("age");
+      Avocado.CsvReader.SetFieldValue(obj, field, "47");
+      Assert.AreEqual(47, obj.age);
+
+      field = obj.GetType().GetField("height");
+      Avocado.CsvReader.SetFieldValue(obj, field, "170.12");
+      Assert.AreEqual(170.12f, obj.height);
+
+      field = obj.GetType().GetField("dob");
+      Avocado.CsvReader.SetFieldValue(obj, field, "13/11/1971");
+      Assert.AreEqual(new System.DateTime(1971, 11, 13), obj.dob);
+    }
+
+    [TestMethod]
+    public void TokensToObj_Basic()
+    {
+      string[] tokens = { "Roger Ramjet", "47", "170.11", "13/11/1971" };
+      PlainObj1 obj;
+
+      obj = new PlainObj1();
+      Avocado.CsvReader.TokensToObj(tokens, obj);
+
+      Assert.AreEqual("Roger Ramjet", obj.name);
+      Assert.AreEqual(47, obj.age);
+      Assert.AreEqual(170.11f, obj.height);
+      Assert.AreEqual(new System.DateTime(1971, 11, 13), obj.dob);
+    }
+
+    [TestMethod]
+    public void ReadLine_Basic()
     {
       Avocado.CsvReader csv;
       PlainObj1 obj;
@@ -32,20 +186,84 @@ namespace UnitTests
 
       Assert.AreEqual("Roger Ramjet", obj.name);
       Assert.AreEqual(47, obj.age);
-      Assert.AreEqual(170.12, obj.height);
+      Assert.AreEqual(170.12f, obj.height);
       Assert.AreEqual(new System.DateTime(1971, 11, 13), obj.dob);
-    }*/
+    }
 
     [TestMethod]
-    public void String_To_Token()
+    public void ReadLines_Basic()
     {
-      string[] tokens;
+      Avocado.CsvReader csv;
+      PlainObj1[] objs;
+      string data;
 
-      tokens = Avocado.CsvReader.StringToTokens("Roger Ramjet, 47, 170.12, 13/11/1971");
-      Assert.AreEqual("Roger Ramjet", tokens[0]);
-      Assert.AreEqual("47", tokens[1]);
-      Assert.AreEqual("170.12", tokens[2]);
-      Assert.AreEqual("13/11/1971", tokens[3]);
+      data =
+        "Roger Ramjet, 47, 170.11, 13/11/1971\n" +
+        "Yank, 27, 160.22, 1/1/1991\n" +
+        "Doodle, 26, 150.33, 2/2/1992\n" +
+        "Dan, 25, 140.44, 3/3/1993";
+      csv = new Avocado.CsvReader(data);
+      objs = csv.ReadLines<PlainObj1>();
+
+      Assert.IsNotNull(objs);
+      Assert.AreEqual(4, objs.Length);
+
+      Assert.AreEqual("Roger Ramjet", objs[0].name);
+      Assert.AreEqual(47, objs[0].age);
+      Assert.AreEqual(170.11f, objs[0].height);
+      Assert.AreEqual(new System.DateTime(1971, 11, 13), objs[0].dob);
+
+      Assert.AreEqual("Yank", objs[1].name);
+      Assert.AreEqual(27, objs[1].age);
+      Assert.AreEqual(160.22f, objs[1].height);
+      Assert.AreEqual(new System.DateTime(1991, 1, 1), objs[1].dob);
+    }
+
+    [TestMethod]
+    public void ReadLines_FromFile()
+    {
+      Avocado.CsvReader csv;
+      PlainObj1[] objs;
+      System.IO.StreamReader file;
+
+      file = new System.IO.StreamReader("data\\sample1.csv");
+      csv = new Avocado.CsvReader(file);
+      objs = csv.ReadLines<PlainObj1>();
+
+      Assert.IsNotNull(objs);
+      Assert.AreEqual(4, objs.Length);
+
+      Assert.AreEqual("Roger Ramjet", objs[0].name);
+      Assert.AreEqual(47, objs[0].age);
+      Assert.AreEqual(170.11f, objs[0].height);
+      Assert.AreEqual(new System.DateTime(1971, 11, 13), objs[0].dob);
+
+      Assert.AreEqual("Yank", objs[1].name);
+      Assert.AreEqual(27, objs[1].age);
+      Assert.AreEqual(160.22f, objs[1].height);
+      Assert.AreEqual(new System.DateTime(1991, 1, 1), objs[1].dob);
+    }
+
+    [TestMethod]
+    public void ReadLines_FromFileLarge()
+    {
+      Avocado.CsvReader csv;
+      PlainObj2[] objs;
+      System.IO.StreamReader file;
+
+      file = new System.IO.StreamReader("data\\sample2.csv");
+      csv = new Avocado.CsvReader(file);
+      csv.hasTitleRow = true;
+      objs = csv.ReadLines<PlainObj2>();
+
+      Assert.IsNotNull(objs);
+      Assert.AreEqual(36634, objs.Length);
+
+      Assert.AreEqual("119736", objs[0].policyID);
+      Assert.AreEqual(498960f, objs[0].eq_site_limit);
+
+      Assert.AreEqual("398149", objs[36633].policyID);
+      Assert.AreEqual(373488.3f, objs[36633].eq_site_limit);
     }
   }
 }
